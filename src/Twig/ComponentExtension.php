d0b7eb670f039e804d428d26cd7cff29cacc87a6
Kevin Bond <kevinbond@gmail.com>
2020-06-30 15:48:35 -0400

    [wip] add attributes function

--- src/Twig/ComponentExtension.php
+++ src/Twig/ComponentExtension.php
@@ -6,12 +6,15 @@ use Symfony\Component\PropertyAccess\Exception\NoSuchPropertyException;
 use Symfony\Component\PropertyAccess\PropertyAccess;
 use Symfony\Component\PropertyAccess\PropertyAccessor;
 use Twig\Extension\AbstractExtension;
+use Twig\TwigFunction;
 
 /**
  * @author Kevin Bond <kevinbond@gmail.com>
  */
 final class ComponentExtension extends AbstractExtension
 {
+    private const ATTRIBUTES_KEY = '_attributes';
+
     private static ?PropertyAccessor $propertyAccessor = null;
     private ComponentRegistry $registry;
 
@@ -25,6 +28,13 @@ final class ComponentExtension extends AbstractExtension
         return [new ComponentTokenParser($this->registry)];
     }
 
+    public function getFunctions(): array
+    {
+        return [
+            new TwigFunction('attributes', [self::class, 'renderAttributes'], ['needs_context' => true, 'is_safe' => ['html']]),
+        ];
+    }
+
     public function getComponentContext(string $name, array $with, array $context): array
     {
         $component = clone $this->registry->get($name);
@@ -33,8 +43,17 @@ final class ComponentExtension extends AbstractExtension
 
         return \array_merge($context, [
             'this' => $component,
-            'attributes' => new AttributeBag($with),
+            self::ATTRIBUTES_KEY => new AttributeBag($with),
         ]);
+    }
+
+    public static function renderAttributes(array $context, array $with = []): string
+    {
+        if (!isset($context[self::ATTRIBUTES_KEY])) {
+            throw new \RuntimeException('Cannot use attributes function outside of component scope.');
+        }
+
+        return $context[self::ATTRIBUTES_KEY]->merge($with);
     }
 
     private static function addContextToComponent(Component $component, array &$context): void
