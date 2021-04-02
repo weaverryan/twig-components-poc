5937bb10160bbae90f0b15b8d8cdc7cd696370c7
Ryan Weaver <ryan@thatsquality.com>
2021-04-02 11:08:05 -0400

    bootstrapping live controller

---
+++ assets/controllers/live_controller.js
@@ -0,0 +1,37 @@
+import { Controller } from 'stimulus';
+
+export default class extends Controller {
+    static values = {
+        component: String,
+        state: Object,
+    }
+
+    /**
+     * Called to update one piece of the model
+     */
+    async update(event) {
+        const model = event.currentTarget.dataset.model;
+        // todo - handle modifiers like "defer"
+
+        const params = new URLSearchParams({
+            component: this.componentValue,
+            state: this.stateValue,
+            model,
+            value: event.currentTarget.value
+        });
+
+        // need to think about the URL structure... I really had this RPC stuff
+        const response = await fetch(`/components?${params.toString()}`);
+        const data = await response.json();
+
+        // TODO - diff the HTML
+        // "html" is the key on the JSON where the HTML is stored
+        this.element.innerHTML = data.html;
+        // "state" holds the new, updated state
+        this.stateValue = data.state;
+    }
+
+    action(event) {
+        console.log('todo');
+    }
+}
