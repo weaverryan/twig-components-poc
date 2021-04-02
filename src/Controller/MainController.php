d544752841cf6a7c39123cb2a946d6f2e33c2d39
Ryan Weaver <ryan@thatsquality.com>
2021-04-02 10:58:32 -0400

    bootstrapping /live page with template only

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -16,4 +16,27 @@ class MainController extends AbstractController
             'controller_name' => 'MainController',
         ]);
     }
+
+    /**
+     * @Route("/live", name="live")
+     */
+    public function live()
+    {
+        // this endpoint mimics what a component would render
+
+        // a component object would normally be turned into an array
+        // by the component system. Done here manually to focus on
+        // the "live" part of the component system
+        $data = [
+            'message' => 'starting message'
+        ];
+
+        return $this->render(
+            'main/live.html.twig',
+            // this initial data part would normally be handled by the
+            // component, taking the component object and turning it into
+            // an array. The important part is how it is passed into Stimulus
+            $data + ['initialData' => $data ]
+        );
+    }
 }
