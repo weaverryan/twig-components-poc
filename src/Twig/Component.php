4ecb6332959f18b45ad86ce673c07de860daa59d
Kevin Bond <kevinbond@gmail.com>
2021-04-05 15:37:27 -0400

    components can inject context into themselves

--- src/Twig/Component.php
+++ src/Twig/Component.php
@@ -2,6 +2,8 @@
 
 namespace App\Twig;
 
+use Symfony\Component\PropertyAccess\PropertyAccess;
+use Symfony\Component\PropertyAccess\PropertyAccessor;
 use function Symfony\Component\String\s;
 
 /**
@@ -9,6 +11,22 @@ use function Symfony\Component\String\s;
  */
 abstract class Component
 {
+    private static ?PropertyAccessor $propertyAccessor = null;
+
+    /**
+     * todo should this be offloaded to a service? should/can the serializer be used?
+     *
+     * @return static
+     */
+    final public function injectContext(array $context): self
+    {
+        foreach ($context as $property => $value) {
+            self::propertyAccessor()->setValue($this, $property, $value);
+        }
+
+        return $this;
+    }
+
     public static function getComponentName(): string
     {
         return s((new \ReflectionClass(static::class))->getShortName())
@@ -21,5 +39,10 @@ abstract class Component
     public static function getComponentTemplate(): string
     {
         return \sprintf('components/%s.html.twig', static::getComponentName());
+    }
+
+    private static function propertyAccessor(): PropertyAccessor
+    {
+        return self::$propertyAccessor ?: self::$propertyAccessor = PropertyAccess::createPropertyAccessor();
     }
 }
--- src/Twig/ComponentExtension.php
+++ src/Twig/ComponentExtension.php
@@ -2,9 +2,6 @@
 
 namespace App\Twig;
 
-use Symfony\Component\PropertyAccess\Exception\NoSuchPropertyException;
-use Symfony\Component\PropertyAccess\PropertyAccess;
-use Symfony\Component\PropertyAccess\PropertyAccessor;
 use Twig\Environment;
 use Twig\Extension\AbstractExtension;
 use Twig\TwigFunction;
@@ -14,9 +11,6 @@ use Twig\TwigFunction;
  */
 final class ComponentExtension extends AbstractExtension
 {
-    private const ATTRIBUTES_KEY = '_attributes';
-
-    private static ?PropertyAccessor $propertyAccessor = null;
     private ComponentRegistry $registry;
 
     public function __construct(ComponentRegistry $registry)
@@ -27,40 +21,17 @@ final class ComponentExtension extends AbstractExtension
     public function getFunctions(): array
     {
         return [
-            new TwigFunction('component', [$this, 'renderComponent'], ['needs_environment' => true, 'needs_context' => true, 'is_safe' => ['all']]),
+            new TwigFunction('component', [$this, 'renderComponent'], ['needs_environment' => true, 'is_safe' => ['all']]),
         ];
     }
 
-    public function getComponentContext(string $name, array $with, array $context): array
+    public function renderComponent(Environment $env, string $name, array $with = []): string
     {
+        // we clone here to ensure we don't modify state of the object in the DI container
         $component = clone $this->registry->get($name);
 
-        self::addContextToComponent($component, $with);
-
-        return \array_merge($context, ['this' => $component]);
-    }
-
-    public function renderComponent(Environment $env, array $context, string $name, array $with = [], bool $withContext = true): string
-    {
-        $context = $this->getComponentContext($name, $with, $withContext ? $context : []);
+        $context = ['this' => $component->injectContext($with)];
 
         return $env->resolveTemplate($context['this']::getComponentTemplate())->render($context);
-    }
-
-    private static function addContextToComponent(Component $component, array &$context): void
-    {
-        foreach ($context as $property => $value) {
-            try {
-                self::propertyAccessor()->setValue($component, $property, $value);
-                unset($context[$property]);
-            } catch (NoSuchPropertyException $e) {
-                continue;
-            }
-        }
-    }
-
-    private static function propertyAccessor(): PropertyAccessor
-    {
-        return self::$propertyAccessor ?: self::$propertyAccessor = PropertyAccess::createPropertyAccessor();
     }
 }
