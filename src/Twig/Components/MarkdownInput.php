c51fffdfe142e392fc27d325841348879702bb3a
Kevin Bond <kevinbond@gmail.com>
2021-04-08 11:46:03 -0400

    added a "readonly" prop to MarkdownInput

--- src/Twig/Components/MarkdownInput.php
+++ src/Twig/Components/MarkdownInput.php
@@ -9,5 +9,17 @@ use App\Twig\LiveComponent;
  */
 final class MarkdownInput extends LiveComponent
 {
+    /**
+     * This is considered a "readonly" property. We don't want the
+     * possibility of this being manipulated during the component's
+     * lifecycle as it affects the field being updated on the
+     * form submit.
+     */
+    public string $name;
+
+    /**
+     * This is considered a "modifiable" property. Manipulating this
+     * in the ajax by hand has no adverse effects on the app.
+     */
     public string $value = '';
 }
